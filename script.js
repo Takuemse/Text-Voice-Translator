4f599a32dfd95fb8a0f894ba71ab1fc7547dc4e4
Takuemse <105425252+Takuemse@users.noreply.github.com>
2024-04-16 09:41:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+  // Get the available voices
+  voices = window.speechSynthesis.getVoices();
+
+  // Set the default voice
+  speech.voice = voices[0];
+
+  // Update the dropdown options
+  voices.forEach((voice, i) => {
+    voiceSelect.options[i] = new Option(voice.name, i);
+  });
+};
+
+voiceSelect.addEventListener("change", () => {
+  speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+});
